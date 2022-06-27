af677bef40b36838ed622ef51d22bda3b0e14743
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-06-27 17:15:10 +0530

    feat: custom hook to handle localStorage

--- src/Hooks/useLocalStorage.js
+++ src/Hooks/useLocalStorage.js
@@ -1,8 +1,16 @@
 export const getDataFromLocal = (key, defaultValue) => {
-    const keyCheck = localStorage.getItem(key);
-    if (keyCheck) {
-      return JSON.parse(keyCheck);
-    } else {
-      return defaultValue;
-    }
-     };
+  const keyCheck = localStorage.getItem(key);
+  if (keyCheck) {
+    return JSON.parse(keyCheck);
+  } else {
+    return defaultValue;
+  }
+};
+
+export const setDataToLocal = (key, value, defaultValue) => {
+  if (value !== undefined) {
+    localStorage.setItem(key, JSON.stringify(value));
+  } else {
+    localStorage.setItem(key, JSON.stringify(defaultValue));
+  }
+};

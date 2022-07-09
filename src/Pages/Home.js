70625d965f0ba2d197a5a2121e24a704b4f0fc5f
Purva sharma <purva08815294379@gmail.com>
2022-07-09 10:37:09 +0530

    style: added navbar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,25 +1,15 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { logout } from "../Auth/auth-slice";
-import { toast } from "react-toastify";
+import { Box, Stack } from "@mui/material";
+import Navbar from "./Navbar";
+import Sidebar from "../Components/SideNavbar/sidebar"
 
 export default function Home() {
-  const dispatch = useDispatch();
   return (
-    <div className="h-100 p-1 ">
-      <h1>Bubble</h1>
-      <button
-        onClick={() => {
-          dispatch(logout(false));
-          toast.success("Successfully Logout!", {
-            toastId: "logout-success",
-            position: toast.POSITION.TOP_RIGHT,
-            autoClose: 2000,
-          });
-        }}
-      >
-        logout
-      </button>
-    </div>
+    <Box>
+      <Navbar/>
+      <Stack>
+      <Sidebar/>
+      </Stack>
+    </Box>
   );
 }

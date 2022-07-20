7232dff94d1312eba77ff57b5fd6191ec034772d
Purva sharma <purva08815294379@gmail.com>
2022-07-20 17:26:16 +0530

    style: changes input field to textarea

--- src/Components/Feed/addPost.js
+++ src/Components/Feed/addPost.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { Box, Stack, Avatar, Input, Button, Card } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import TextareaAutosize from "@mui/base/TextareaAutosize";
 import { addPosts } from "../../Features/post-slice";
 import { useDispatch } from "react-redux";
-import {toast} from "react-toastify"
+import { toast } from "react-toastify";
+import {getDataFromLocal} from "../../Hooks/useLocalStorage";
 
 function AddPost() {
   const dispatch = useDispatch();
   const [content, setContent] = useState("");
+  const uname = getDataFromLocal('user','u')
 
   const changeHandler = (e) => {
     setContent(e.target.value);
@@ -27,7 +30,8 @@ function AddPost() {
         toastId: "empty-field-failed",
         position: toast.POSITION.TOP_RIGHT,
         autoClose: 2000,
-      });    }
+      });
+    }
   };
   return (
     <Box m={2}>
@@ -40,9 +44,10 @@ function AddPost() {
           justifyContent="center"
         >
           <Avatar sx={{ bgcolor: "red" }} aria-label="avatar">
-            G
+          {uname.username.slice()[0].toUpperCase()}
           </Avatar>
-          <Input
+          <TextareaAutosize
+            aria-label="minimum height"
             onChange={changeHandler}
             placeholder="What's On your mind??"
             sx={{ flex: 2 }}

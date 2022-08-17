517ef15767fba00457d7d964d3112af3c03a7373
Purva sharma <purva08815294379@gmail.com>
2022-08-17 16:06:37 +0530

    fix: currUser dp

--- src/Pages/CurrUserProfile.js
+++ src/Pages/CurrUserProfile.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import UserCard from "../Components/Users/UserCard";
 import {Box} from "@mui/material";
-import {getDataFromLocal} from '../Hooks/useLocalStorage'
+import {getDataFromLocal} from '../Hooks/useLocalStorage';
+import { getUserById, getUser } from '../Features/user-slice';
+import {useSelector,useDispatch} from "react-redux";
+import {toast} from 'react-toastify';
+
+
 
 function UserProfile() {
-    const currUser = getDataFromLocal('user',{})
+    const user = getDataFromLocal('user',{})
+    const {userInfo,userError} = useSelector(getUser)
+    const dispatch = useDispatch()
+    const [currUser,setCurrUser] = useState(userInfo)
+
+    useEffect(()=>{
+      dispatch(getUserById(user._id))
+      setCurrUser(userInfo)
+      // eslint-disable-next-line
+    },[userInfo])
+    useEffect(() => {
+      if (userError === undefined) {
+        toast.error("something went wrong!! try again later", {
+          toastId: "user-error-failed",
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 2000,
+        });
+      } else {
+        toast.error(userError, {
+          toastId: "user-error-failed",
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 2000,
+        });
+      }
+    }, [userError]);
   return (
     <Box flex={6} className="h-100" color="text.primary">
         <UserCard
